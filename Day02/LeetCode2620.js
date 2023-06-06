8cb873dcff880db5ea6c5c8f591126d225e053f3
Balaji Durshetti <durshettibalaji1@gmail.com>
2023-06-06 21:50:17 +0530

    added day2 code

---
+++ Day02/LeetCode2620.js
@@ -0,0 +1,10 @@
+var createCounter = function(n) {
+    return function() {
+        console.log(n)
+        return n++
+    };   
+};
+ const counter = createCounter(10)
+ counter() 
+ counter() 
+ counter() 
